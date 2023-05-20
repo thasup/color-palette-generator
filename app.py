997d87bf1e7c9fa375ab56ea1eb00355c02935d1
thasup <thanachonfirst@hotmail.com>
2023-05-21 05:35:32 +0700

    Add palette endpoint

--- app.py
+++ app.py
@@ -11,15 +11,46 @@ app = Flask(
     template_folder="templates"
 )
 
-@app.route("/")
-def index():
+def get_and_render_prompt(text):
+    message=f"""
+    You're a color paletter generating assistant that responds to <Text> prompt for color palettes.
+    You must generate color palettes that fit the theme, mood, tone, or instructions in the <Text> prompt.
+    You must not generate the same color twice in palettes.
+
+    The palettes should be between 2 to 8 colors. 
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Text: {text}
+    Result:
+    """
+
     completion = openai.Completion.create(
             model="text-davinci-003",
-            prompt="give me some joke",
+            prompt=message,
             max_tokens=100,
-            temperature=1,
+            temperature=0.8,
         )
-    return completion.choices[0].text
+
+    colors = json.loads(completion.choices[0].text)
+    return colors
+
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
+    # GET QUERY FROM THE FORM
+    query = app.logger.info(request.form.get("query"))
+
+    # OPEN AI COMPLETION CALL
+    colors = get_and_render_prompt(query)
+
+    # PRINT RETURN COLORS
+    app.logger.info(colors)
+
+    # RETURN LIST OF COLORS
+    return {"colors": colors}
+
+@app.route("/")
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
