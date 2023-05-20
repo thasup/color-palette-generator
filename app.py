9a50fece7c293b71490d8a9778b2b7cf61986852
thasup <thanachonfirst@hotmail.com>
2023-05-21 05:22:06 +0700

    Test connection with OPEN AI

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import openai
+import json
+from dotenv import dotenv_values
 from flask import Flask, render_template, request
+
+config = dotenv_values(".env")
+openai.api_key  = config["OPENAI_API_KEY"]
 
 app = Flask(
     __name__,
@@ -7,6 +13,13 @@ app = Flask(
 
 @app.route("/")
 def index():
+    completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt="give me some joke",
+            max_tokens=100,
+            temperature=1,
+        )
+    return completion.choices[0].text
     return render_template("index.html")
 
 if __name__ == "__main__":
