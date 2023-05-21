1ecf6f7f36d6d2ec579cca6ede0ee9b65ed49b9b
thasup <thanachonfirst@hotmail.com>
2023-05-21 13:54:48 +0700

    Adjust prompt to also return color names

--- app.py
+++ app.py
@@ -13,13 +13,19 @@ app = Flask(
 
 def get_and_render_prompt(text):
     message=f"""
-    You're a color palette generating assistant that responds to <Text> prompt for color palettes.
-    You must generate color palettes that fit the theme, mood, tone, or instructions in the <Text> prompt.
-    You must not generate the same color twice in palettes.
+    You are a color palette generating assistant that responds to <Text> prompt for color palettes.
+    Your goal is to generate highly accurate color palettes that align with the given theme, mood, tone, or instructions in the <Text> prompt.
 
-    The palettes should be between 2 to 8 colors.
+    Key Guidelines:
+    - Generate color palettes that closely match the specified theme, mood, tone, or instructions in the <Text> prompt.
+    - Avoid duplicating colors within the same palette.
+    - Keep the palettes within a range of 2 to 8 colors.
+    - Do not give "null" as a result, try produce some color palette.
 
-    Desired Format: a JSON array of hexadecimal color codes
+    Desired Format:
+      - a JSON array of hexadecimal color codes
+      - a JSON array of color name respectively
+      - for example: "[\"#f00\", \"#ff0\", \"#0f0\", \"#0ff\", \"#00f\", \"#f0f\", \"#000\"], [\"red\", \"yellow\", \"green\", \"aqua\", \"blue\", \"fuchsia\", \"black\"]"
 
     Text: {text}
     Result:
@@ -28,12 +34,29 @@ def get_and_render_prompt(text):
     completion = openai.Completion.create(
             model="text-davinci-003",
             prompt=message,
-            max_tokens=100,
-            temperature=1,
+            max_tokens=200,
+            temperature=0.8,
         )
 
-    colors = json.loads(completion.choices[0].text)
-    return colors
+    result = completion.choices[0].text.strip()
+
+    # Split the result into two parts: colors and names
+    colors, names = result.split("], [")
+
+    colors = f"""{colors}]"""
+    names = f"""[{names}"""
+
+    try:
+      colors = json.loads(colors)
+      names = json.loads(names)
+
+      result_dict = {
+          "colors": colors,
+          "names": names
+      }
+      return result_dict
+    except json.decoder.JSONDecodeError as e:
+      print("Error decoding JSON:", e)
 
 @app.route("/palette", methods=["POST"])
 def prompt_to_palette():
@@ -41,13 +64,14 @@ def prompt_to_palette():
     query = app.logger.info(request.form.get("query"))
 
     # OPEN AI COMPLETION CALL
-    colors = get_and_render_prompt(query)
+    result = get_and_render_prompt(query)
 
     # PRINT RETURN COLORS
-    app.logger.info(colors)
 
     # RETURN LIST OF COLORS
-    return {"colors": colors}
+    return {
+        "result": result
+       }
 
 @app.route("/")
 def index():
